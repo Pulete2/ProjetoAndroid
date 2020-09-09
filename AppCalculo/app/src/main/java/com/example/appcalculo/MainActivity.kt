435175f9f5c422c91c5ebe663dceea960765da1d
Pulete2 <paulo_andres97@hotmail.com>
2020-09-09 18:29:44 -0300

    Front do app

--- AppCalculo/app/src/main/java/com/example/appcalculo/MainActivity.kt
+++ AppCalculo/app/src/main/java/com/example/appcalculo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.appcalculo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun verificarResposta(componente:View){
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+
+        resposta.visibility = View.VISIBLE
+
+        val pets = ed_pets.text.toString().toInt()
+        val numeroInteiro = ed_campo1.text.toString().toInt()
+        val valor = ed_campo2.text.toString()
+
+        if (numeroInteiro <= 0){
+            ed_campo1.hint = "Valor invalido"
+            campo_1.setTextColor(Color.RED)
+        }
+
+        if (pets == 0){
+            
+        }
     }
 }
